e8a3c4b71b9052e65ab890f58fec6b1abf904be1
NeoGuRi95 <jea5158@gmail.com>
2022-07-27 10:54:50 +0900

    수정 요청시 수정 폼을 화면에 출력 & 기존 내용을 수정 폼에 담기

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -31,6 +31,9 @@ public class DispatchServlet extends HttpServlet {
                     case "/usr/article/delete":
                         articleController.doDelete(rq);
                         break;
+                    case "/usr/article/modify":
+                        articleController.showModify(rq);
+                        break;
                     case "/usr/member/login":
                         memberController.showLogin(rq);
                         break;
--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -23,13 +23,24 @@ public class ArticleController {
         rq.view("usr/article/write");
     }
 
-    public void doWrite(Rq rq) {
-        String title = rq.getParam("title", "");
-        String body = rq.getParam("body", "");
 
-        long id = articleService.write(title, body);
+    public void showModify(Rq rq) {
+        long id = rq.getLongPathValueByIndex(1, 0);
 
-        rq.appendBody("%d번 게시물이 생성 되었습니다.".formatted(id));
+        if (id == 0) {
+            rq.appendBody("번호를 입력해주세요.");
+            return;
+        }
+
+        ArticleDto articleDto = articleService.findById(id);
+
+        if (articleDto == null) {
+            rq.appendBody("해당 글이 존재하지 않습니다.");
+            return;
+        }
+
+        rq.setAttr("article", articleDto);
+        rq.view("usr/article/modify");
     }
 
     public void showDetail(Rq rq) {
@@ -49,6 +60,15 @@ public class ArticleController {
 
         rq.setAttr("article", articleDto);
         rq.view("usr/article/detail");
+    }
+
+    public void doWrite(Rq rq) {
+        String title = rq.getParam("title", "");
+        String body = rq.getParam("body", "");
+
+        long id = articleService.write(title, body);
+
+        rq.appendBody("%d번 게시물이 생성 되었습니다.".formatted(id));
     }
 
     public void doDelete(Rq rq) {
