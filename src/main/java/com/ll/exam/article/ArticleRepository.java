02ee6c584973ab45d01fab832077cddc431367af
NeoGuRi95 <jea5158@gmail.com>
2022-07-27 11:27:07 +0900

    Test Datat 추가

--- src/main/java/com/ll/exam/article/ArticleRepository.java
+++ src/main/java/com/ll/exam/article/ArticleRepository.java
@@ -4,6 +4,7 @@ import com.ll.exam.article.dto.ArticleDto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class ArticleRepository {
     private static List<ArticleDto> datum;
@@ -12,6 +13,16 @@ public class ArticleRepository {
     static {
         datum = new ArrayList<>();
         lastId = 0;
+
+        makeTestData();
+    }
+
+    private static void makeTestData() {
+        IntStream.rangeClosed(1, 10).forEach(id -> {
+            String title = "제목%d".formatted(id);
+            String body = "내용%d".formatted(id);
+            write(title, body);
+        });
     }
 
     public static long write(String title, String body) {
